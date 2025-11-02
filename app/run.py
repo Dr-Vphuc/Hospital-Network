f926a6afdfb2a897f58dba3746680d9ae6963a7b
Dr-Vphuc <ngphuc689@gmail.com>
2025-11-02 09:26:06 +0700

    first merge hduong111`

--- app/run.py
+++ app/run.py
@@ -1,10 +1,9 @@
 from app import create_app
-
 app = create_app()
 
 @app.route('/')
 def root():
-    return "<h1>We will get A+</h1>"
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)
