f360075018e68d668e71f8ad473d9bfb27f1c4f6
Dr-Vphuc <ngphuc689@gmail.com>
2025-12-17 00:52:19 +0700

    add run.py

--- app/backend/__init__.py
+++ app/backend/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from .db import db
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +10,13 @@ def create_app():
         template_folder=os.path.join(BASE_DIR, '..', 'frontend', 'templates'),
         static_folder=os.path.join(BASE_DIR, '..', 'frontend', 'static'),
     )
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql+pymysql://root:@localhost:3306/hospital_network'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
 
+    db.init_app(app)
+    
+    
     return app
 
