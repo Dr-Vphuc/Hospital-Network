9b656d399efbdafbe28b2fa7cbb68e2cbb4f4cd7
huynguyenkhac17 <23001525@hus.edu.vn>
2026-01-03 23:50:21 +0700

    chore: load environment from .env file

--- app/backend/__init__.py
+++ app/backend/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_login import LoginManager
 from .db import db
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
@@ -13,18 +18,22 @@ def create_app():
         template_folder=os.path.join(BASE_DIR, '..', 'frontend', 'templates'),
         static_folder=os.path.join(BASE_DIR, '..', 'frontend', 'static'),
     )
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'mysql+pymysql://root:@localhost:3306/hospital'
+
+     # Load config from environment variables
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = '22092005271020052025'
     
     from .api.admin import admin_bp
     app.register_blueprint(admin_bp)
+
     from .api.auth.routes import auth_bp
     app.register_blueprint(auth_bp)
+
     from .api.user.routes import user_bp
     app.register_blueprint(user_bp)
 
+    # Init extensions
     db.init_app(app)
     login_manager.init_app(app)
     
